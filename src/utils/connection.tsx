9c74d634266525a4394852488cee29da4b0469ea
Rudy <lengxin5348@gmail.com>
2021-09-24 23:25:20 +0800

    fix rpc

--- src/utils/connection.tsx
+++ src/utils/connection.tsx
@@ -7,8 +7,8 @@ import { ConnectionContextValues, EndpointInfo } from './types';
 
 
 export const endpoints = [
-  { url: 'https://raydium.rpcpool.com', weight: 30 },
-  { url: 'https://solana-api.projectserum.com', weight: 70 }
+  { url: 'https://raydium.rpcpool.com', weight: 100 },
+  // { url: 'https://solana-api.projectserum.com', weight: 70 }
 ]
 
 export function getRandomEndpoint() {
