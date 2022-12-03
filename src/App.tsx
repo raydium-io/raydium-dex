4700133cda681fde43f6474e253b36ff9f7fa080
Rudy <lengxin5348@gmail.com>
2022-12-03 20:23:37 +0800

    update note

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,13 @@ export default function App() {
   return (
     <Suspense fallback={() => <Spin size="large" />}>
       <GlobalStyle />
-      <div style={{background: 'red', padding: '0 10px', fontWeight: 'bold', textAlign: 'center' }}>
-      <p>New order creation is currently disabled. Please cancel and settle any existing orders.</p>
-      <p>This trading page is a UI implementation that accesses the Project Serum DEX <a style={{color: '#6bcdd6'}} href='https://github.com/project-serum/serum-dex'>smart contracts</a>.</p>
-      <p>Serum can still be accessed from other protocols listed on the <a  style={{color: '#6bcdd6'}} href='https://portal.projectserum.com/'>Serum website</a>. For more info, click <a  style={{color: '#6bcdd6'}} href='https://docs.raydium.io/raydium/updates/serum-dex-pause'>here</a>.</p>
+      <div style={{background: '#9B1DAF', padding: '10px 20px', fontWeight: 'bold' }}>
+        <div style={{width: '100%', maxWidth: '750px', margin: '0 auto', lineHeight: '8px'}}>
+          <p>This page is a UI implementation that accesses the Project Serum DEX <a style={{color: '#39D0D8'}} href='https://github.com/project-serum/serum-dex'>smart contracts</a>.</p>  
+          <p>This trading page will be deprecated on December 14 - Please cancel and settle existing orders.</p> 
+          <p>All other Raydium features (i.e. swaps, pools, farms, etc) are unaffected and operate as normal.</p> 
+          <p>For more info and other resources for accessing Serum, click <a  style={{color: '#39D0D8'}} href='https://docs.raydium.io/raydium/updates/serum-dex-pause'>here</a>.</p>
+        </div>
       </div>
       <ErrorBoundary>
         <ConnectionProvider>
